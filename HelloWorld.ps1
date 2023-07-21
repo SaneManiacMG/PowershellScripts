ab073accbd44bf4b413c91057d027c5a9bd461b9
SaneManiacMG <mmoroane@hotmail.com>
2023-07-21 07:33:43 +0200

    Finally and escape sequences do not work in ps1 Using an array for the list of colors

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -3,20 +3,6 @@ Write-Host "Hello World!"
 $Name = Read-Host -Prompt "What is your name"
 Write-Host "Hello $Name!"
 
-# Get user input
-Write-Host "What's your favorite color?"
-Write-Host "1. Red"
-Write-Host "2. Blue"
-Write-Host "3. Green"
-$color = Read-Host -Prompt "Select a number"
-switch ($color) {
-    1 { Write-Host "You selected Red" }
-    2 { Write-Host "You selected Blue" }
-    3 { Write-Host "You selected Green" }
-    default { Write-Host "You did not select a valid color" }
-}
-Write-Host ""
-
 # check user number
 $Number = Read-Host -Prompt "Enter a number"
 if ($Number -cge 50 -and $Number -cle 100) {
@@ -31,6 +17,13 @@ elseif ($Number -cle 25) {
 else {
     Write-Host "You did not enter a number in the range of 25 to 50"
 }
-finally {
-    Write-Host "You entered $Number\n"
+
+# create an array
+$ArrayOfColors = @("Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Black", "White");
+Write-Host "Select a color from the list"
+for ($i = 0; $i -lt $ArrayOfColors.Length; $i++) {
+    Write-Host "$i. $($ArrayOfColors[$i])"
 }
+$color = Read-Host -Prompt "Select a number"
+Write-Host "You selected $($ArrayOfColors[$color])"
+
